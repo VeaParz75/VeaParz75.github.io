47d66454b7ddc0d4ff9a3b30ab4bd81991b1afb4
Vea Iyer <iyer94@purdue.edu>
2024-02-12 18:27:54 -0500

    name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Website</h1>
+        <h1>Vaishnavi (Vea) Iyer</h1>
       </header>
       <main>
         <section>
