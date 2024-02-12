ff3e9e4d56d86dd68cea3862cf23fc790bf2980c
Vea Iyer <iyer94@purdue.edu>
2024-02-12 18:16:46 -0500

    did this work

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and heyy did this work because then this is where i edit.
         </p>
         <a
           className="App-link"
