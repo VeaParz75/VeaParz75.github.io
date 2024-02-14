5d74a1473b540604ccc4979526e3f54a54a6111e
Vea Iyer <iyer94@purdue.edu>
2024-02-14 00:50:31 -0500

    fixed Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,31 +3,13 @@ import ProjectImg from "../assets/wip.png"; // Import your project image
 
 const Projects = () => {
   return (
-    <section className="bg-secondary text-white px-5 py-32" id="projects">
-      <div className="container mx-auto">
-        <div className="flex flex-col items-center justify-center">
-          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
-            My Projects
-          </h2>
-
-          <p className="pb-5">
-            Here you can find a showcase of some of my projects.
-          </p>
-          <p className="pb-5">
-            Feel free to explore and get inspired!
-          </p>
-          <p>
-            You can also visit my GitHub profile for more projects.
-          </p>
-        </div>
-
-        <div className="mt-10 flex justify-center">
-          <img
-            src={ProjectImg}
-            alt="project illustration"
-            style={{ maxWidth: "50%" }} // Adjust the maximum width here
-          />
-        </div>
+    <section className="bg-secondary text-white flex flex-col items-center justify-center h-screen" id="projects">
+      <div className="mt-10">
+        <img
+          src={ProjectImg}
+          alt="project illustration"
+          style={{ maxWidth: "45%", display: "block", margin: "0 auto" }} // Adjust the maximum width and center the image
+        />
       </div>
     </section>
   );
