202a443945277d4bcf9dbd4616d37f2df6dc393c
Vea Iyer <iyer94@purdue.edu>
2025-11-20 14:25:44 -0500

    browserrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home'; // Import the Home component
 import About from './components/About';
